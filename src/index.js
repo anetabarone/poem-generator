909b693d634aedd2479e2a2fddf13a7f1d329b50
anetabarone <anetabarone@yahoo.com>
2024-02-06 19:07:11 +0100

    corrected the form ID

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ function generatePoem(event) {
   event.preventDefault();
 
   new Typewriter("#poem", {
-    strings: ["basnicka basnicka"],
+    strings: "basnicka basnicka basnicka",
     autoStart: true,
     delay: 1,
     cursor: "",
   });
-
-  poemElement.innerHTML = "basnicka";
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
